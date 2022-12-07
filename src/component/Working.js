e2a5f853e6e3a6e5fd92aa009a80ce9a674ad1e7
tyoung <taeyoung917@gmail.com>
2022-12-07 16:39:02 +0900

    delete 추가

--- src/component/Working.js
+++ src/component/Working.js
@@ -1,18 +1,29 @@
+import { useState } from 'react';
 import './Todo.css';
 
 export default function Working({ text, title }) {
+  const [conDelete, setConDelete] = useState([...text, ...title]);
+
+  function deleteHandler() {
+    console.log('삭제');
+    setConDelete('');
+  }
   return (
     <div className="working_wrapper">
       <h2>Working...</h2>
       <ul className="work_list">
-        <li className={text ? 'list_active' : 'list'}>
-          <h3>{title}</h3>
-          <p>{text}</p>
-          <div className="btn_wrap">
-            <button className="btn_del">삭제하기</button>
-            <button className="btn_com">완료하기</button>
-          </div>
-        </li>
+        {conDelete && (
+          <li className={text ? 'list_active' : 'list'}>
+            <h3>{title}</h3>
+            <p>{text}</p>
+            <div className="btn_wrap">
+              <button className="btn_del" onClick={deleteHandler}>
+                삭제하기
+              </button>
+              <button className="btn_com">완료하기</button>
+            </div>
+          </li>
+        )}
       </ul>
     </div>
   );
